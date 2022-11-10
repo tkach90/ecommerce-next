04f4ab397a8a685be9825949ec99b57be31f799f
tkach90 <tkach90@yahoo.com.ua>
2022-11-10 23:19:00 +0200

    create a basic next project structure connect server with mock graphql data based on shopify structure fetch products from mock database

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,30 @@
+import getAllProductsQuery from "../product/utils/queries/get-all-products";
+
+type FetchParams = {
+	query: string
+}
+
+const fetchApi = async ({query}: FetchParams) => {
+	const url = "http://localhost:4000/graphql";
+
+	const res = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			query
+		})
+	});
+
+	const data = await res.json();
+
+	return { data };
+}
+
+const getAllProducts = async (): Promise<any[]> => {
+	const products = await fetchApi({query: getAllProductsQuery});
+	return products.data;
+}
+
+export default getAllProducts;
